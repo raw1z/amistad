7488535f7fe6594d454d403e7c61f05632709058
Rawane ZOSSOU <dev@raw1z.fr>
2010-09-12 23:37:17 +0200

    ensure uniqueness of the friendship between two users

--- lib/amistad/friendship_model.rb
+++ lib/amistad/friendship_model.rb
@@ -9,6 +9,7 @@ module Amistad
         belongs_to :friend, :class_name => "User", :foreign_key => "friend_id"
         
         validates_presence_of :user_id, :friend_id
+        validates_uniqueness_of :friend_id, :scope => :user_id
       end
     end
     
