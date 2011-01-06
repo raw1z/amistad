dcbd761f3df9858293164e759207d4a7d6f614e3
Adrian Dulić <adulic@gmail.com>
2011-01-06 14:55:45 +0100

    Force table creation in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define do
     t.string :name, :null => false
   end
 
-  create_table :friendships do |t|
+  create_table :friendships, :force => true do |t|
     t.integer :user_id
     t.integer :friend_id
     t.integer :blocker_id
