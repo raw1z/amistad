621bd3fe7ae52b56f893958f98875519c31eef25
Rawane ZOSSOU <dev@raw1z.fr>
2012-02-29 09:09:24 +0100

    create a file for containing all the configuration logic

--- lib/amistad.rb
+++ lib/amistad.rb
@@ -1,5 +1,6 @@
 require 'active_support/concern'
 require 'active_support/dependencies/autoload'
+require 'amistad/config'
 
 module Amistad
   extend ActiveSupport::Autoload
@@ -10,24 +11,4 @@ module Amistad
   autoload :FriendshipModel
   autoload :FriendModel
   autoload :Friendship
-
-  class << self
-    attr_accessor :friend_model
-
-    def configure
-      yield self
-    end
-
-    def friend_model
-      @friend_model || 'User'
-    end
-
-    def friendship_model
-      "#{self.friend_model}Friendship"
-    end
-
-    def friendship_class
-      Amistad::Friendship.const_get(self.friendship_model)
-    end
-  end
 end
---
+++ lib/amistad/config.rb
@@ -0,0 +1,21 @@
+module Amistad
+  class << self
+    attr_accessor :friend_model
+
+    def configure
+      yield self
+    end
+
+    def friend_model
+      @friend_model || 'User'
+    end
+
+    def friendship_model
+      "#{self.friend_model}Friendship"
+    end
+
+    def friendship_class
+      Amistad::Friendship.const_get(self.friendship_model)
+    end
+  end
+end
