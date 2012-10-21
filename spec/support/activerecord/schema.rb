1b22f337e980cb68ccf751f278b647346cecac16
Rawane Zossou <dev@raw1z.fr>
2012-10-21 04:46:24 +0200

    allow to test with sqlite, mysql and postgresql

--- spec/support/activerecord/schema.rb
+++ spec/support/activerecord/schema.rb
@@ -1,7 +1,22 @@
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => ":memory:"
-)
+def db_config(type)
+  YAML.load_file(File.expand_path('../database.yml', __FILE__))[type]
+end
+
+def connect_server(type)
+  config = db_config type
+  database = config['database']
+
+  ActiveRecord::Base.establish_connection config.merge('database' => (type == 'postgresql' ? 'postgres' : nil))
+  ActiveRecord::Base.connection.recreate_database(database)
+  ActiveRecord::Base.establish_connection(config) 
+end
+
+case ENV['RDBM']
+when 'mysql' then connect_server 'mysql'
+when 'postgresql' then connect_server 'postgresql'
+else
+  ActiveRecord::Base.establish_connection db_config('sqlite')
+end
 
 class CreateSchema < ActiveRecord::Migration
   def self.up
