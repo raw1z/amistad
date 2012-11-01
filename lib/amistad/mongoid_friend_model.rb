ef2a68d879631609d49aa593d41446b44f1c5d8b
Rawane Zossou <dev@raw1z.fr>
2012-11-01 23:06:48 +0100

    fix issue #26
    Default values seems to be working fine on mongoid now

--- lib/amistad/mongoid_friend_model.rb
+++ lib/amistad/mongoid_friend_model.rb
@@ -45,14 +45,6 @@ module Amistad
         :blocked_pending_friend_ids,
         :blocked_pending_inverse_friend_ids,
       )
-
-      %w(friend_ids inverse_friend_ids pending_friend_ids pending_inverse_friend_ids blocked_friend_ids blocked_inverse_friend_ids blocked_pending_friend_ids blocked_pending_inverse_friend_ids).each do |attribute|
-        define_method(attribute.to_sym) do
-          value = read_attribute(attribute)
-          write_attribute(attribute, value = []) if value.nil?
-          value
-        end
-      end
     end
   end
 end
