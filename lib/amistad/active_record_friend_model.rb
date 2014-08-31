d7de7b445ba61b933539b7c035a530d5e76caf32
Gregorio Setti <gregorio.setti@gmail.com>
2014-08-31 21:58:01 +0200

    Replace :conditions with new syntax

--- lib/amistad/active_record_friend_model.rb
+++ lib/amistad/active_record_friend_model.rb
@@ -15,12 +15,12 @@ module Amistad
       has_many  :pending_invited,
         :through => :friendships,
         :source => :friend,
-        :conditions => { :'friendships.pending' => true, :'friendships.blocker_id' => nil }
+        -> { where(:'friendships.pending' => true, :'friendships.blocker_id' => nil)}
 
       has_many  :invited,
         :through => :friendships,
         :source => :friend,
-        :conditions => { :'friendships.pending' => false, :'friendships.blocker_id' => nil }
+        -> { where(:'friendships.pending' => false, :'friendships.blocker_id' => nil)}
 
       #####################################################################################
       # inverse friendships
@@ -32,12 +32,12 @@ module Amistad
       has_many  :pending_invited_by,
         :through => :inverse_friendships,
         :source => :friendable,
-        :conditions => { :'friendships.pending' => true, :'friendships.blocker_id' => nil }
+        ->  { where(:'friendships.pending' => true, :'friendships.blocker_id' => nil)}
 
       has_many  :invited_by,
         :through => :inverse_friendships,
         :source => :friendable,
-        :conditions => { :'friendships.pending' => false, :'friendships.blocker_id' => nil }
+        -> { where(:'friendships.pending' => false, :'friendships.blocker_id' => nil)}
 
       #####################################################################################
       # blocked friendships
@@ -49,12 +49,12 @@ module Amistad
       has_many  :blockades,
         :through => :blocked_friendships,
         :source => :friend,
-        :conditions => "friend_id <> blocker_id"
+        -> {where("friend_id <> blocker_id")}
 
       has_many  :blockades_by,
         :through => :blocked_friendships,
         :source => :friendable,
-        :conditions => "friendable_id <> blocker_id"
+        -> {where("friendable_id <> blocker_id")}
     end
 
     # suggest a user to become a friend. If the operation succeeds, the method returns true, else false
